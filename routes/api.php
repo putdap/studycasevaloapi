c30670e0de08833ae3b91a8805d661ccc54f5a60
Daffa Putra <197006038@stundet.unsil.ac.id>
2022-04-02 16:47:48 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/val/content/v1/contents', [\App\Http\Controllers\WrapperController::class, 'contents'])-> middleware('npm')->name(name: 'contents');
 Route::get('/val/ranked/v1/leaderboards/by-act/{actId}', [\App\Http\Controllers\WrapperController::class, 'leaderboardsByActId'])-> middleware('npm')->name(name: 'leaderboardsByActId');
-Route::get('/val/status/v1/platform-data', [\App\Http\Controllers\WrapperController::class, 'platformdata'])->name(name: 'platformdata');
+Route::get('/val/status/v1/platform-data', [\App\Http\Controllers\WrapperController::class, 'platformdata'])-> middleware('npm')->name(name: 'platformdata');
 Route::get('/v1/bundles', [\App\Http\Controllers\WrapperController::class, 'bundles'])-> middleware('npm')->name(name: 'bundles');
 Route::get('/v1/maps', [\App\Http\Controllers\WrapperController::class, 'maps'])-> middleware('npm')->name(name: 'maps');
 Route::get('/user/identitas', function (){
