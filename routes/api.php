68e839b9ab8a70edbc77af7d941d6815c2191110
Daffa Putra <197006038@stundet.unsil.ac.id>
2022-04-02 13:45:52 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/val/content/v1/contents', [\App\Http\Controllers\WrapperController::class, 'contents'])->name(name: 'contents');
-Route::get('/val/ranked/v1/leaderboards/by-act/{actId}', [\App\Http\Controllers\WrapperController::class, 'leaderboardsByActId'])->name(name: 'leaderboardsByActId');
+Route::get('/val/content/v1/contents', [\App\Http\Controllers\WrapperController::class, 'contents'])-> middleware('npm')->name(name: 'contents');
+Route::get('/val/ranked/v1/leaderboards/by-act/{actId}', [\App\Http\Controllers\WrapperController::class, 'leaderboardsByActId'])-> middleware('npm')->name(name: 'leaderboardsByActId');
 Route::get('/val/status/v1/platform-data', [\App\Http\Controllers\WrapperController::class, 'platformdata'])->name(name: 'platformdata');
-Route::get('/v1/bundles', [\App\Http\Controllers\WrapperController::class, 'bundles'])->name(name: 'bundles');
-Route::get('/v1/maps', [\App\Http\Controllers\WrapperController::class, 'maps'])->name(name: 'maps');
+Route::get('/v1/bundles', [\App\Http\Controllers\WrapperController::class, 'bundles'])-> middleware('npm')->name(name: 'bundles');
+Route::get('/v1/maps', [\App\Http\Controllers\WrapperController::class, 'maps'])-> middleware('npm')->name(name: 'maps');
 Route::get('/user/identitas', function (){
     return [
         'code' => '0',
